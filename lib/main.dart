3917b2d8b14005e7b93207c5467851669f349250
gabu-hero <164854308+gabu-hero@users.noreply.github.com>
2024-11-22 12:29:17 +0530

    Splash Screen Part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:feedback_app/mainloginpage.dart';
 import 'package:feedback_app/splashscreen.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +5,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
