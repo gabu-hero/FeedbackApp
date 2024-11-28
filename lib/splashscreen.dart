d324cd419b65685fed207575664a4adaddbaa2c5
gabu-hero <164854308+gabu-hero@users.noreply.github.com>
2024-11-28 22:37:50 +0530

    App Logo and Name

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,10 +1,8 @@
 import 'package:feedback_app/mainloginpage.dart';
 import 'package:flutter/material.dart';
 
-// ignore: use_key_in_widget_constructors
 class SplashScreen extends StatefulWidget {
   @override
-  // ignore: library_private_types_in_public_api
   _SplashScreenState createState() => _SplashScreenState();
 }
 
